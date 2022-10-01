fae2825ef7eaa2f83cbbb8cb15741ac87416d3ff
MarinaMts <marina.martinss_@outlook.com>
2022-09-30 22:43:54 -0300

    CRIAÇÃO MODAL E AJUSTES

--- public/js/chamada.js
+++ public/js/chamada.js
@@ -17,6 +17,23 @@ selects.forEach(element => {
       selectText.innerHTML = this.textContent;
       listElement.classList.toggle("hide");
       arrowIcon.classList.toggle("rotate");
-      }
+      } 
   }
 });
+
+function iniciaModal (modalID) {
+  const modal = document.getElementById(modalID);
+  if(modal) {
+    modal.classList.add('mostrar');
+    modal.addEventListener('click', (evento) => {
+      if(evento.target.className == 'fechar'){
+        modal.classList.remove('mostrar');
+      }
+    });
+  }
+}
+
+// const iniciarChamada = document.querySelector('.iniciar');
+//iniciarChamada.addEventListener('click', () => iniciaModal('modal-chamada'));
+
+iniciaModal('modal-chamada');
