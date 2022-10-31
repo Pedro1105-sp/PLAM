c8981d1386fce36d0b4695161a1db2248cbfacc4
Pedro Henrique Alves <pedro.henriquerealalvessp@gmail.com>
2022-10-31 12:19:40 -0300

    AJUSTES LOGIN API

--- route.js
+++ route.js
@@ -2,7 +2,9 @@ const express = require('express');
 const res = require('express/lib/response');
 const router = express.Router();
 const db = require('./db');
-const dbIndex = require('./db/index.js');
+const mysql = require("./mysql").pool
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 
 router.get(
@@ -70,5 +72,73 @@ router.post(
         )
     }
 );
+
+router.post(
+    "/cadastrarAluno", (req, res)=>{
+        mysql.getConnection((err, conn) =>{
+            if(err){return res.status(500).send({error:error})}
+            bcrypt.hash(req.body.SENHA_ALUNO, 10, (errBcrypt, hash)=>{
+                if(errBcrypt) {return res.status(500).send({error: errBcrypt})}
+                conn.query(
+                    `insert into tb_usuario_alunos (EMAIL_ALUNO, SENHA_ALUNO, ID_ALUNO_USUARIO) values (?, ?, ?);`,
+                    [req.body.EMAIL_ALUNO, hash, req.body.ID_ALUNO_USUARIO],
+                    (error, results)=>{
+                        conn.release();
+                        if(error) {return res.status(500).send({error:error})}
+                        response ={
+                            mensagem: "USUARIO  CRIADO COM SUCESSO!!",
+                            usuarioCriado: {
+                                ID_ALUNO_USUARIO: req.body.ID_ALUNO_USUARIO,
+                                EMAIL_ALUNO: req.body.EMAIL_ALUNO
+                            }
+                        }
+                        return res.status(201).send(response)
+                    }
+                )
+            })
+        })
+    }
+)
+
+router.post ("/login", (req, res, next)=>{
+    const senha = req.body.senha
+    mysql.getConnection((err, conn) =>{
+        if(err){return res.status(299).send({err:err})}
+        conn.query(
+            `SELECT * FROM tb_usuario_alunos WHERE ID_ALUNO_USUARIO = ?;`,
+            [req.body.ID_ALUNO_USUARIO],
+            (error, results)=>{
+                conn.release();
+                if(error) {return res.status(299).send({error:error})}
+                if(results.length < 1){
+                    return res.status(299).send({mensagem: "RM NÃO ENCONTRADO"})
+                }
+                bcrypt.compare(senha, results[0].senha, (err, result)=>{
+                    if(err) { 
+                        const token = jwt.sign({
+                            ID_ALUNO_USUARIO: results[0].ID_ALUNO_USUARIO,
+                            EMAIL_ALUNO: results[0].EMAIL_ALUNO
+                        }, "SEGREDO", {
+                            expiresIn: "1h"
+                        }
+                        )
+                        return res.status(200).send({
+                            mensagem: "SENHA OKAY!",
+                            token: token
+                        })
+                        
+                    }
+                    if(result){
+                        return res.status(299).send({mensagem: "FALHA AUTENTICAÇÃO SENHA!"});
+                    }
+                    return res.status(299).send({mensagem: "FALHA  SENHA!"});
+                });
+
+            }
+        )
+            })
+        })
+
+
 
 module.exports = router;
