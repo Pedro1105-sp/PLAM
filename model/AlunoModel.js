b0e7affa282efec1100d1e14a6ddf9e29c02950f
Pedro Henrique Alves <pedro.henriquerealalvessp@gmail.com>
2022-11-24 19:35:03 -0300

    ALUNO UPDATE BACK

---
+++ model/AlunoModel.js
@@ -0,0 +1,51 @@
+const Sequelize = require("sequelize");
+
+const connection = require("../database/database");
+
+/*Importação da tabela de categoria para criação da chave estrangeira
+representanto a cardinalidade*/
+const Turma = require('./TurmaModel');
+
+const Aluno = connection.define(
+    "tb_alunos",
+    {
+        RM:{
+            type: Sequelize.INTEGER(10).UNSIGNED,
+            allowNull: false,
+            primaryKey: true
+        },
+        NM_ALUNO:{
+            type: Sequelize.STRING(45),
+            allowNull: false
+        },
+        CPF_ALUNO:{
+            type: Sequelize.STRING(11),
+            allowNull: false
+        },
+        TEL_ALUNO:{
+            type: Sequelize.STRING(11),
+            allowNull: false
+        },
+        EMAIL_ALUNO: {
+            type: Sequelize.STRING(70),
+            allowNull: false
+        },
+        SENHA_ALUNO: {
+            type: Sequelize.STRING(45),
+            allowNull: false
+        }
+        
+    }
+);
+
+/*Implementação da  CHAVE ESTRANGEIRA - LADO N*/
+
+Turma.hasMany(Aluno, {
+  foreignKey: 'ID_TURMA_ALUNO'
+});
+
+
+
+//Aluno.sync({force:true});
+
+module.exports = Aluno;
